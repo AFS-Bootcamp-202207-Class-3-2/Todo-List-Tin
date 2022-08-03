8b9e96d6365b59fb4a381979aa8c012b9e003c51
chenqitian <369077336@qq.com>
2022-08-03 22:02:00 +0800

    refactor: menu change

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,29 +1,34 @@
 import { Menu } from "antd";
 import { NavLink, Outlet } from "react-router-dom";
-import { EditOutlined, QuestionOutlined, CheckOutlined} from '@ant-design/icons';
+import {
+  EditOutlined,
+  QuestionOutlined,
+  CheckOutlined,
+} from "@ant-design/icons";
 import "./index.css";
 
 export default function Layout() {
+  const menuItems = [
+    {
+      key: "home",
+      icon: <EditOutlined />,
+      label: <NavLink to="/">Home</NavLink>,
+    },
+    {
+      key: "help",
+      icon: <QuestionOutlined />,
+      label: <NavLink to="/help">Help Page</NavLink>,
+    },
+    {
+      key: "done",
+      icon: <CheckOutlined />,
+      label: <NavLink to="/done">Done Page</NavLink>,
+    },
+  ];
   return (
     <div>
       <nav>
-        <Menu mode="horizontal" defaultSelectedKeys={["home"]}>
-          <Menu.Item key="home" icon={<EditOutlined />}>
-            <NavLink to="/">
-              Home
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="help" icon={<QuestionOutlined />}>
-            <NavLink  to="/help">
-              Help Page
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="done" icon={<CheckOutlined />}>
-            <NavLink  to="/done">
-              Done Page
-            </NavLink>
-          </Menu.Item>
-        </Menu>
+        <Menu items={menuItems} mode="horizontal" />
       </nav>
       <Outlet />
     </div>
